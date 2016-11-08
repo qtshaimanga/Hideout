d2e2843b0ee7d8ba55cb5438eeb59d07903300fe
quentin.tshaimanga <q.tshaimanga@gmail.com>
2016-11-08 21:17:34 +0100

    refactoring structure

---
+++ test/vue/vue-secret/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import store from './store'
+
+export default {
+  store: store,
+  data () {
+    return {
+
+    }
+  },
+  mounted: function() {
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./styles/fonts.scss";
+@import "./styles/transition.scss";
+
+  html, body{
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+  }
+
+  #app{
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+  }
+
+</style>
---
+++ test/vue/vue-secret/src/containers/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <navigation></navigation>
+    <webgl-home></webgl-home>
+  </div>
+</template>
+
+<script>
+import WebglHome  from '../components/WebglHome';
+import Navigation from '../components/Navigation';
+
+export default {
+  components: {
+    WebglHome,
+    Navigation
+  },
+  vuex: {
+    getters: {},
+    actions: {}
+  },
+  data () {
+    return {}
+  },
+	mounted: function() {
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .home{
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+  }
+
+</style>
